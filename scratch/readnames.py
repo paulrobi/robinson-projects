0f41542f9a00dac483bd052229398889714459dc
Paul Robinson <Paul.Robinson@dowjones.com>
2021-11-04 15:58:21 -0400

    Update readnames.py

--- scratch/readnames.py
+++ scratch/readnames.py
@@ -5,5 +5,15 @@ with open('names.csv', 'r') as csv_file:
      csv_reader = csv.reader(csv_file)
      next(csv_reader)
 
+#     for line in csv_reader:
+#         print(line[1])
+
+
+### using Dict
+with open('names.csv', 'r') as csv_file:
+     csv_reader = csv.DictReader(csv_file)
+#     next(csv_reader)
+
      for line in csv_reader:
-         print(line[1])
+         print(line['email'])
+~
