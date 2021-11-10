2b2a7bcb7afb09a0677a3c934e8e7e48563d9298
Paul Robinson <Paul.Robinson@dowjones.com>
2021-11-10 15:14:05 -0500

    nov 10

--- scratch/iam_policy_versions-testing.py
+++ scratch/iam_policy_versions-testing.py
@@ -1,7 +1,7 @@
 # iam delete policy versions
 # delete only the customer managed policies versions older then $days_to_delete not AWS policy versions (Scope = 'Local')
 # debug output to be removed
-# robinsonp 11/6/2021
+# robinsonp 11/7/2021
 #https://www.slsmk.com/use-boto3-to-assume-a-role-in-another-aws-account/
 
 # 186630241196,Robinson-Home,nct_cse_prod_tools_role
@@ -16,19 +16,29 @@ import sys
 import csv
 import botocore
 sts_client = boto3.client('sts')
+date_format_str = "%Y%m%d-%H%M%S"
 
 iam_ob = boto3.client("iam")
 date_now = datetime.now()
-days_to_delete = 45
+days_to_delete = 30
 
 boto_sts=boto3.client('sts')
 
-with open('iam_policy_versions-template.csv', 'r') as csv_file_input:
+action = "debug"
+dryrun = "True"
+
+outputfile = open('iam_policy_versions-output.csv', 'w')
+output_header = "AccountId,AccountName,PolicyName,IsDefaultVersion,PolicyAge,PolicyCreateDate,ProfileVersion,Action,DryRun,AssumedRoleError\n"
+outputfile.write(output_header)
+
+with open('iam_policy_versions-accounts.csv', 'r') as csv_file_input:
      csv_reader = csv.DictReader(csv_file_input)
 
      for line in csv_reader:
          dest_account_id = line['AccountId']
          dest_role_name = line['DestRoleName']
+         dest_account_env = line['AccountEnv']
+         dest_account_name = line['AccountName']
          try:
             stsresponse = boto_sts.assume_role(
                 RoleArn=("arn:aws:iam::{acctnumber}:role/{acctname}".format(acctnumber=dest_account_id, acctname=dest_role_name)),
@@ -38,7 +48,6 @@ with open('iam_policy_versions-template.csv', 'r') as csv_file_input:
             newsession_id = stsresponse["Credentials"]["AccessKeyId"]
             newsession_key = stsresponse["Credentials"]["SecretAccessKey"]
             newsession_token = stsresponse["Credentials"]["SessionToken"]
-#           print ("newsession_id {}:newsession_key{}:newsession_token{}".format(newsession_id,newsession_key,newsession_token))
             iam_assumed_client = boto3.client(
                   'iam',
                   aws_access_key_id=newsession_id,
@@ -50,24 +59,47 @@ with open('iam_policy_versions-template.csv', 'r') as csv_file_input:
                 response = iam_ob.list_policy_versions(
                     PolicyArn=(iam_policy['Arn'])
                 )
+                policy_name=(iam_policy['PolicyName'])
+                print("Policy Name IS: {}".format (policy_name))
                 for policy_ver in response['Versions']:
+                    policy_create_date = (policy_ver['CreateDate'])
+                    formatted_policy_create_date = policy_create_date.replace(tzinfo=None)
+                    delta_time = date_now - formatted_policy_create_date
+
                     if policy_ver['IsDefaultVersion'] is True:
-                         pass
+                         policy_isdefault="True"
                     else:
-                         policy_create_date = (policy_ver['CreateDate'])
-                         formatted_policy_create_date = policy_create_date.replace(tzinfo=None)
-                         delta_time = date_now - formatted_policy_create_date
+                         policy_isdefault="False"
                          if delta_time.days >= days_to_delete:
-                             print(f"DEFAULT={policy_ver['IsDefaultVersion']}  DELETING=True {policy_ver['VersionId']} DaysOld: [ {delta_time.days} ] {iam_policy['Arn']}")
+                            action="delete"
                          else:
-                             print(f"DEFAULT={policy_ver['IsDefaultVersion']}  DELETING=False {policy_ver['VersionId']} DaysOld: [ {delta_time.days} ] {iam_policy['Arn']}")
+                            action="skip"
+                            print("Generic Pass")
+
+                assumerole_error = "SuccessAssumeRole"
+                output_row = "{},{},{},{},{},{},{},{},{},{}\n".format(
+                            dest_account_id, dest_account_name, policy_name,
+                            policy_isdefault, formatted_policy_create_date, policy_ver['VersionId'],
+                            delta_time,action,dryrun, assumerole_error
+                 )
+                outputfile.write(output_row)
          except botocore.exceptions.ClientError as e:
-                  print("Client Error")
+                assumerole_error = "ErrorAssumeRole"
+                output_row = "{},{},{},{},{},{},{},{},{},{}\n".format(
+                            dest_account_id, dest_account_name, policy_name,
+                            policy_isdefault, formatted_policy_create_date, policy_ver['VersionId'],
+                            delta_time,action,dryrun, assumerole_error
+                 )
+                outputfile.write(output_row)
+                print("Client Error")
+         except botocore.exceptions.NoSuchEntityException as e:
+                assumerole_error = "ErrorAssumeRole"
+                output_row = "{},{},{},{},{},{},{},{},{},{}\n".format(
+                            dest_account_id, dest_account_name, policy_name,
+                            policy_isdefault, formatted_policy_create_date, policy_ver['VersionId'],
+                            delta_time,action,dryrun, assumerole_error
+                 )
+                outputfile.write(output_row)
+                print("NoSuchEntityException Client Error")
 
-
-###     with open('output-iam_policy_versions-outout.csv', 'w') as iamveroutput:
-###          fieldnames = ['AccountId','AccountName','DestRoleName']
-###          csv_writer = csv.DictWriter(iamveroutput, fieldnames=fieldnames, delimiter=',')
-###          csv_writer.writeheader()
-###          for line in csv_reader:
-###              csv_writer.writerow(line)
+outputfile.close()
