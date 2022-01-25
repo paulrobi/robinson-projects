94cdfb8fbf0a15ec0120923b41f677e4822af191
Paul Robinson <Paul.Robinson@dowjones.com>
2022-01-25 18:05:05 -0500

    Update new_iam_removed_unused.py

--- iamversionremoval/new_iam_removed_unused.py
+++ iamversionremoval/new_iam_removed_unused.py
@@ -1,3 +1,12 @@
+# Requires AssumeRole with
+#                "iam:ListInstanceProfilesForRole",
+#                "iam:DetachRolePolicy",
+#                "iam:DeleteRolePolicy",
+#                "iam:ListAttachedRolePolicies",
+#                "iam:GenerateServiceLastAccessedDetails",
+#                "iam:RemoveRoleFromInstanceProfile",
+#                "iam:ListRoles",
+#                "iam:ListRolePolicies"
 # Read CSV for acount and Role info
 # assume role for each account
 # -log error on role assume
@@ -34,6 +43,7 @@
 import boto3, botocore
 import subprocess, os, sys, argparse, datetime, time, csv
 from time import mktime
+from csv import reader
 from datetime import datetime, timedelta
 boto_sts=boto3.client('sts')
 client = boto3.client("iam")
@@ -45,8 +55,17 @@ days_to_delete = 30
 default_policy_age_min = 30
 max_days_since_used = 30
 dest_acctnumber="186630241196"
-dest_role_name="nct_cse_prod_tools_role"
+dest_assume_role_name="nct_cse_prod_tools_role"
 #arn:aws:iam::186630241196:role/nct_cse_prod_tools_role
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--csv_input', default='18JAN2022-mytest-roles.csv')
+args = parser.parse_args()
+
+if os.path.isfile(args.csv_input):
+   pass
+else:
+   sys.exit(f'Inputfile {args.csv_input} does not exist')
 
 # create output csv
 outputfile = open('iam_rm_role_policies_output.csv', 'w',newline='')
@@ -57,30 +76,33 @@ outputfile.write(output_header)
 # assume role for account function
 def perform_assume_role(account_info):
     stsresponse = boto_sts.assume_role(
-      RoleArn=("arn:aws:iam::{}:role/{}".format(dest_acctnumber,dest_role_name)),
+      RoleArn=("arn:aws:iam::{}:role/{}".format(dest_acctnumber,dest_assume_role_name)),
          RoleSessionName='iam_pol_ver_session',
          ExternalId='terraform_agent_request',
          DurationSeconds=900
      )
+    ###x print(stsresponse["Credentials"]["AccessKeyId"])
     return (stsresponse["Credentials"]["AccessKeyId"],stsresponse["Credentials"]["SecretAccessKey"],stsresponse["Credentials"]["SessionToken"])
 
 def perform_remove_role_from_instance_profile(rolename):
     print(f'Entered>>> perform_remove_role_from_instance_profile {rolename}')
     inst_profile = client.list_instance_profiles_for_role(RoleName=rolename)
     for inst_profile in inst_profile['InstanceProfiles']:
-        print(f'Status Role {rolename} Removed from InstanceProfile {inst_profile}')
-        res2 = client.remove_role_from_instance_profile( RoleName=rolename, InstanceProfileName=inst_profile['InstanceProfileName'])
+        pass
+        ###x print(f'Status Role {rolename} Removed from InstanceProfile {inst_profile}')
+        ###x res2 = client.remove_role_from_instance_profile( RoleName=rolename, InstanceProfileName=inst_profile['InstanceProfileName'])
 
 def perform_remove_instance_profile(rolename):
     print("****************************")
     paginator = client.get_paginator('list_instance_profiles_for_role')
     for response in paginator.paginate(RoleName=rolename):
         for instanceProfile in response['InstanceProfiles']:
-            print(f'Status InstanceProfileToRemoved {instanceProfile}')
-            client.remove_role_from_instance_profile(
-                    RoleName=rolename,
-                    InstanceProfileName=instanceProfile['InstanceProfileName']
-            )
+            pass
+            ###x print(f'Status InstanceProfileToRemoved {instanceProfile}')
+            ###x client.remove_role_from_instance_profile(
+            ###x         RoleName=rolename,
+            ###x         InstanceProfileName=instanceProfile['InstanceProfileName']
+            ###x )
     print("****************************")
 
 
@@ -90,14 +112,15 @@ def perform_policy_detach(rolename):
     for attached_policy in attached_policies_response:
         attached_policy_name = attached_policy['PolicyName']
         attached_policy_arn = attached_policy['PolicyArn']
-        print(f'RoleName={rolename} PolicyName={attached_policy_name} PolicyArn={attached_policy_arn}')
-        print(f'Status AttachedPolicyDetached = {attached_policy}')
-        attached_policy_detach = client.detach_role_policy(RoleName=rolename,PolicyArn=attached_policy_arn)
+        ###X print(f'RoleName={rolename} PolicyName={attached_policy_name} PolicyArn={attached_policy_arn}')
+        print(f'RoleName={rolename} PolicyName={attached_policy_name}')
+        ###X print(f'Status AttachedPolicyDetached = {attached_policy}')
+        ###x attached_policy_detach = client.detach_role_policy(RoleName=rolename,PolicyArn=attached_policy_arn)
     attached_inline_policies_response = client.list_role_policies(RoleName=rolename)['PolicyNames']
     for attached_inline_policy in attached_inline_policies_response:
-        print(f'RoleName={rolename} InlinePolicyName={attached_inline_policy}')
-        print(f'Status AttachedInlinePolicyDetached = {attached_inline_policy}')
-        client.delete_role_policy(RoleName=rolename,PolicyName=attached_inline_policy)
+        print(f'RoleName={rolename} InlinePolicyName = {attached_inline_policy}')
+        ###x print(f'Status AttachedInlinePolicyDetached = {attached_inline_policy}')
+        ###x client.delete_role_policy(RoleName=rolename,PolicyName=attached_inline_policy)
 
 
 #list roles in selected account mark those over X days since accessed
@@ -129,7 +152,7 @@ def perform_role_maintenance(session_id,session_key,session_token):
                 perform_policy_detach(rolename)
                 perform_remove_role_from_instance_profile(rolename)
                 perform_remove_instance_profile(rolename)
-                client.delete_role(RoleName=rolename)
+                ###x client.delete_role(RoleName=rolename)
             else:
                rolelastused = min(last_accessed_date)
                days_since_used = (date_now - rolelastused.replace(tzinfo=None)).days
@@ -139,14 +162,31 @@ def perform_role_maintenance(session_id,session_key,session_token):
                     perform_policy_detach(rolename)
                     perform_remove_role_from_instance_profile(rolename)
                     perform_remove_instance_profile(rolename)
-                    client.delete_role(RoleName=rolename)
+                    ###x client.delete_role(RoleName=rolename)
                else:
                      action_taken ="Skip-Role-Deletion:"
                      print(f'{myarn} days since used = {days_since_used} less then {maxdays_since_used} --skip')
                continue
 
 # MAIN
-session_id,session_key,session_token = perform_assume_role([('dest_acctnumber',dest_acctnumber),('dest_role_name',dest_role_name)])
-iam_assumed_client = boto3.client('iam', aws_access_key_id=session_id, aws_secret_access_key=session_key, aws_session_token=session_token)
-print('\n\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
-perform_role_maintenance(session_id,session_key,session_token)
+# perform read of input csv
+with open(args.csv_input, 'r') as csv_file_input:
+     csv_reader = csv.DictReader(csv_file_input)
+     for line in csv_reader:
+         newsession_token = "None"
+         policy_name = "AssumeRoleError"
+         policy_isdefault = "AssumeRoleError"
+         formatted_policy_create_date = "AssumeRoleError"
+         dest_account_id = line['cloud_provider_id']
+         dest_account_name = line['account_name']
+         dest_role_name = line['asset_name']
+
+         with open('aws_account_assumed_roles.csv', 'r') as csv_acct_file_input:
+             csv_acct_reader = csv.DictReader(csv_acct_file_input)
+             for acct_file_line in csv_acct_reader:
+                 if acct_file_line['accountnumber'] == line['cloud_provider_id']:
+                    ###x print(f'dest_account_id::{dest_account_id} dest_account_name::{dest_account_name} dest_role_name::{dest_role_name}')
+                    session_id,session_key,session_token = perform_assume_role([('dest_acctnumber',dest_acctnumber),('dest_assume_role_name',dest_assume_role_name)])
+                    iam_assumed_client = boto3.client('iam', aws_access_key_id=session_id, aws_secret_access_key=session_key, aws_session_token=session_token)
+                    print('\n\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+                    perform_role_maintenance(session_id,session_key,session_token)
